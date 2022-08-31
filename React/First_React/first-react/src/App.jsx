6de0eb58fea999307a1421bff56b00424e6d9f85
Dashawn <creswellshawn@gmail.com>
2022-08-31 10:35:04 -0700

    First React

---
+++ React/First_React/first-react/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <fieldset>
+      <legend>App</legend>
+      <fieldset>
+        <legend>child</legend>
+      </fieldset>
+    </fieldset>
+  );
+}
+
+export default App;
