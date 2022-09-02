639a763f89d5c9014c055751b763ea5392fd360f
Dashawn <creswellshawn@gmail.com>
2022-09-02 10:04:55 -0700

    box generator

--- React/box-generator/src/App.jsx
+++ React/box-generator/src/App.jsx
@@ -10,7 +10,7 @@ import styles from './components/box.css'
 
 function App() {
   const [newColor, setNewColor] = useState("");
-  const [newSize, setNewSize] = useState(1);
+  const [newSize, setNewSize] = useState(100);
   const [boxes, setBoxes] = useState([]);
 
   const handleNewBox = (event) => {
@@ -27,50 +27,65 @@ function App() {
 
     setBoxes([...boxes, boxItem])
     setNewColor("")
-    setNewSize(1)
+    setNewSize(100)
   }
 
   return (
-    <div>
-      <form onSubmit={(event) => { handleNewBox(event); }}>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className='card'>
+            <div className="card-header">
+              <h1>Create Box</h1>
+            </div>
+            <div className="card-body">
+              <form onSubmit={(event) => { handleNewBox(event); }}>
+                <div>
+                  <label>Color: </label>
+                  <input
+                  className='form-control'
+                    onChange={(event) => { setNewColor(event.target.value) }}
+                    type="text"
+                    value={newColor}
+                  />
+                </div>
+                <div>
+                  <label>Size in Pixels: </label>
+                  <input
+                  className='form-control'
+                    onChange={(event) => { setNewSize(event.target.value) }}
+                    type="number"
+                    min="100"
+                    value={newSize}
+                  />
+                </div>
+                <div className='d-flex justify-content-center align-items-center'>
+                  <button className='btn btn-primary mt-4'>Add</button>
+                </div>
+              </form>
+            </div>
 
-        <div>
-          <label>Color: </label>
-          <input
-            onChange={(event) => { setNewColor(event.target.value) }}
-            type="text"
-            value={newColor}
-          />
-        </div>
-        <div>
-          <label>Size in Pixels: </label>
-          <input
-            onChange={(event) => { setNewSize(event.target.value) }}
-            type="number"
-            min="1"
-            value={newSize}
-          />
-        </div>
-        <div>
-          <button>Add</button>
-        </div>
-      </form>
+            <div className={styles.container}>
+              {
+                boxes.map((box, index) => {
 
-      <div className={styles.container}>
-        {
-          boxes.map((box, index) => {
+                  return (
+                    <Color
+                      key={index}
+                      box={box}
+                    />
+                  )
+                }
+                )
+              }
+            </div>
+          </div>
 
-            return (
-              <Color
-                key={index}
-                box={box}
-              />
-            )
-          }
-          )
-        }
+        </div>
       </div>
     </div>
+
+
   );
 }
 
--- React/box-generator/src/components/BoxDisplay.jsx
+++ React/box-generator/src/components/BoxDisplay.jsx
@@ -2,7 +2,7 @@ const Color = (props) => {
 
     return (
 
-        <div style={{ backgroundColor: props.box.color, width: props.box.size + 'px', height: props.box.size + 'px' }}></div>
+            <div style={{ backgroundColor: props.box.color, width: props.box.size + 'px', height: props.box.size + 'px' }}></div>
 
 
     );
